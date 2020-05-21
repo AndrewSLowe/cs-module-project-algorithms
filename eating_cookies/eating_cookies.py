b8983a46fb863ac74f1d69b6d1efc8276e2bd4c4
Andrew S Lowe <andrewstevenlowe@gmail.com>
2020-05-21 13:55:26 -0400

    mvp

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,47 @@
-'''
-Input: an integer
-Returns: an integer
-'''
-def eating_cookies(n):
-    # Your code here
+# First pass!!
+def eating_cookies(n):   # 3**n operations
+    # base case: when there are no more cookies
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    # This represents our recursive case where there are still cookies to be eaten
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
-    pass
+# cachine/memoization: let's save our work so we don't have
+# to recompute it again
+
+# Need some sort of data structure where we'll save the data
+# arrays and dictionaries
+# If we check our cache and see that the answer we're looking for is
+# already in there, just return that answer
+# what if the cache doesn't have what we're looking for? Or how do we
+# get answers in there?
+# There's going to be a first time for caluclating an answer, and we're
+# going to do it the same way we're currently doing it. 
+
+def eating_cookies(n, cache=None):
+    print(n)
+    # base case: when there are no more cookies
+    if n == 0:
+        return 1
+    # check for negative n values
+    elif n < 0:
+        return 0
+    # init our cache if we don't have it yet
+    # add a case to have us check the cache
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = [0 for _ in range(n+1)]
+        # we can go ahead and save our answer to the cache
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    # This represents our recursive case where there are still cookies to be eaten
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 500
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,41 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+# first pass
+def single_number(nums):
+    # first-pass solution
+    # we'll keep an array, call it `no_dups` to hold numbers we see in the nums array
+    no_dups = []
+    # interate through nums
+    for x in nums:
+        # check to see if the number is already in the `no_dups` array
+        if x not in no_dups:
+            no_dups.append(x)
+        # if it is, remove it from the `no_dups` array
+        else:
+            no_dups.remove(x)
+    # when we're done iterating, the only number in our `no_dups` array is the 
+    # odd number out
+    # return it
+    return no_dups
 
-    pass
-
+# improved version
+def single_number(nums):
+    # first-pass solution
+    # we'll keep an array, call it `no_dups` to hold numbers we see in the nums array
+    no_dups = []
+    # interate through nums
+    for x in nums:
+        # check to see if the number is already in the `no_dups` array
+        if x not in no_dups:
+            no_dups.append(x)
+        # if it is, remove it from the `no_dups` array
+        else:
+            no_dups.remove(x)
+    # when we're done iterating, the only number in our `no_dups` array is the 
+    # odd number out
+    # return it
+    return no_dups
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
